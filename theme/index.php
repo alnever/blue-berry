b560042092f0ef118eaa0adb455370184fa7f283
Alex Neverov <al_neverov@live.ru>
2019-02-27 05:05:03 -0800

    Initial init

---
+++ theme/index.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * Index of the Blue Berry Theme
+ *
+ * The file contains the definitions of the main loop of the theme
+ *
+ * @package SWTK
+ * @subpackage blue_berry
+ * @since Funny Colors 1.0
+ */
+ ?>
+<?php get_header(); ?>
+
+
+  <div class="swtk-container">
+    <div class="swtk-main">
+        <?php get_template_part('nav'); ?>
+        <?php get_template_part('head'); ?>
+
+        <!-- header sidebar -->
+        <div class="swtk-horizontal-sidebar swtk-header-sidebar">
+          <?php dynamic_sidebar('header-sidebar'); ?>
+        </div>
+
+        <!-- content -->
+        <div class="swtk-content">
+            <!-- left sidebar -->
+            <div class="swtk-vertical-sidebar swtk-left-sidebar">
+                <?php wp_nav_menu([
+                        'theme_location' => 'secondary',
+                        'depth' => 3,
+                        'container_class' => 'vertical-menu',
+                    ]);
+                ?>
+                <?php dynamic_sidebar('left-sidebar'); ?>
+            </div>
+
+            <!-- posts -->
+            <div class="swtk-posts">
+                <!-- sticky post -->
+                <div class="swtk-sticky-posts">
+                    <?php
+                        $sticky = get_option('sticky_posts');
+                        $args = [
+                            'post__in' => $sticky,
+                            'ignore_sticky_posts' => 1,
+                        ];
+                        $query = new WP_Query($args);
+                        while ( $query->have_posts() ): $query->the_post();
+                    ?>
+                        <article class="swtk-post-sticky" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                            <div class="swtk-post-image">
+                                <?php if (has_post_thumbnail()):  ?>
+                                    <a href="<?php echo get_permalink(); ?>">
+                                        <img src="<?php esc_url( the_post_thumbnail_url() ); ?>" alt="">
+                                    </a>
+                                <?php endif; ?>
+                            </div>
+                            <!-- post text -->
+                            <div class="swtk-post-text">
+                                <div class="swtk-post-title">
+                                    <a href="<?php echo get_permalink(); ?>">
+                                        <?php
+                                            if (get_the_title() == "") {
+                                                echo get_the_date("F d, Y");
+                                            } else {
+                                                the_title();
+                                            }
+                                         ?>
+                                    </a>
+                                </div>
+                                <!-- post before main text -->
+                                <div class="swtk-post-author">
+                                    <img class="swtk-icon" src="<?php echo get_theme_file_uri('/icons/author.svg'); ?>"/ >
+                                    <?php the_author_link(); ?>
+                                </div>
+                                <div class="swtk-post-date">
+                                    <?php echo get_the_date('F d, Y'); ?>
+                                </div>
+                                <div class="swtk-post-content">
+                                    <?php the_excerpt(); ?>
+                                </div>
+                                <?php if (has_category()): ?>
+                                    <div class="swtk-post-categories">
+                                        <img src="<?php echo get_theme_file_uri('/icons/category.svg') ?>" class="swtk-icon">
+                                        <?php the_category(); ?>
+                                    </div>
+                                <?php endif; ?>
+                                <?php if (has_tag()): ?>
+                                    <div class="swtk-post-tags">
+                                        <img src="<?php echo get_theme_file_uri('/icons/tags.svg') ?>" class="swtk-icon">
+                                        <?php the_tags('','',''); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        </article>
+                    <?php
+                        endwhile;
+                        wp_reset_postdata();
+                    ?>
+                </div>
+
+                <!-- main posts loop -->
+                <?php
+                    $args = [
+                        'posts_per_page' => 9,
+                        'ignore_sticky_posts' => 1,
+                    ];
+                    $query = new WP_Query($args);
+                ?>
+                <?php if ($query->have_posts()): ?>
+                    <div class="swtk-row">
+
+                          <?php while($query->have_posts()): $query->the_post(); ?>
+                            <article class="swtk-post-index" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                                <div class="swtk-post-image">
+                                    <?php if (has_post_thumbnail()):  ?>
+                                        <a href="<?php echo get_permalink(); ?>">
+                                            <img src="<?php esc_url( the_post_thumbnail_url() ); ?>" alt="">
+                                        </a>
+                                    <?php endif; ?>
+                                </div>
+                                <!-- post header -->
+                                <div class="swtk-post-title">
+                                    <a href="<?php echo get_permalink(); ?>">
+                                        <?php
+                                            if (get_the_title() == "") {
+                                                echo get_the_date("F d, Y");
+                                            } else {
+                                                the_title();
+                                            }
+                                         ?>
+                                    </a>
+                                </div>
+                                <!-- post before main text -->
+                                <div class="swtk-post-author">
+                                    <img class="swtk-icon" src="<?php echo get_theme_file_uri('/icons/author.svg'); ?>"/ >
+                                    <?php the_author_link(); ?>
+                                </div>
+                                <div class="swtk-post-date">
+                                    <?php echo get_the_date('F d, Y'); ?>
+                                </div>
+                                <div class="swtk-post-content">
+                                    <?php the_excerpt(); ?>
+                                </div>
+                                <?php if (has_category()): ?>
+                                    <div class="swtk-post-categories">
+                                        <img src="<?php echo get_theme_file_uri('/icons/category.svg'); ?>" class="swtk-icon">
+                                        <?php the_category(); ?>
+                                    </div>
+                                <?php endif; ?>
+                                <?php if (has_tag()): ?>
+                                    <div class="swtk-post-tags">
+                                        <img src="<?php echo get_theme_file_uri('/icons/tags.svg'); ?>" class="swtk-icon">
+                                        <?php the_tags('','',''); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </article>
+                          <?php endwhile; ?>
+                          <div class="swtk-pagination">
+                              <img src="<?php echo get_theme_file_uri('/icons/pages.svg'); ?>" alt="" class="swtk-icon">
+                              <?php the_posts_pagination(); ?>
+                          </div>
+                    </div>
+                <?php endif; ?>
+            </div>
+            <!-- posts -->
+
+            <!-- right sidebar -->
+            <div class="swtk-vertical-sidebar swtk-right-sidebar">
+                <?php dynamic_sidebar('right-sidebar'); ?>
+            </div>
+        </div>
+
+
+
+  <?php get_footer(); ?>
---
+++ theme/readme.txt
@@ -0,0 +1,43 @@
+=== Blue Berry ===
+Contributors: Alex Neverov
+Tags: one-column, right-sidebar, flexible-header, custom-colors, custom-header, custom-menu, custom-logo, featured-images, footer-widgets, post-formats, sticky-post, theme-options, threaded-comments, translation-ready
+Requires at least: 5.0.3
+Tested up to: 5.0.3
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The theme created using SASS WordPress Kit
+
+== Description ==
+
+The theme created using SASS WordPress Kit
+
+== Frequently Asked Questions ==
+
+= A question that someone might have =
+
+An answer to that question.
+
+== Changelog ==
+
+= 0.5 =
+* Menu style changed. The main menu depth is changed to 1.
+* Layout and color scheme were modified.
+* The gray scale filter was applied to the post images.
+* Sticky posts were excluded form the main loop.
+* The limit of the sticky posts published before the main loop was canceled.
+
+= 0.1 =
+* The theme was initially generated using the kit.
+* The structure of the base styles, variables and mixins was changed according the expirience of the recent using of the kit.
+* All necessary identifications where replaced.
+* Posts images animation was added.
+* Header structure was advanced: a place for social menu was reserved.
+
+
+
+== Resources ==
+
+SASS WordPress Theming Kit http://github.com/alnever/sass-wordpress-theming-kit
+Icons are from the free icon set https://linearicons.com/free
